df4ae51c061bafbd5dee504c00c7da774da8ac18
panostaja <panostaja@gmail.com>
2022-04-17 09:23:05 +0300

    ennen poistamista

--- laturi/src/laturi/Ajoneuvot.java
+++ laturi/src/laturi/Ajoneuvot.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.Scanner;
 
 import fi.jyu.mit.ohj2.WildChars;
